816db3bb3164560037be3a76e21edef1b9c8605f
Paddybarn <paddybarn21@gmail.com>
2022-09-04 10:49:40 -0400

    one more time!

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-app.get('/*', (req, res) =>
+app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
