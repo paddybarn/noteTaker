f6d0da5067ca7ddd44bc52174b292e73b2896181
Paddybarn <paddybarn21@gmail.com>
2022-09-04 09:52:26 -0400

    added working get route for notes.html

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 const express = require('express');
-const api = require('./routes/indexhtml');
+const api = require('./routes/indexroute');
 const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', api);
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
+// app.use(express.static('pages'));
 
-app.get('*', (req, res) =>
+app.get('/*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+  res.sendFile(path.join(__dirname, './db/db.json'))
 );
 
 app.listen(PORT, () => {
